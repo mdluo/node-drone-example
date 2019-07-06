f382e00e9995e7d65ca7e8d4324444626d2a0f42
Mingdong Luo <mdluo@live.com>
2019-07-06 17:27:27 +0800

    feat: add express

---
+++ src/routers.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', function(req, res, next) {
+  res.status(200).json({});
+});
+
+router.get('/users', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+module.exports = router;
---
+++ src/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const createError = require('http-errors');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const routers = require('./routers');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/', routers);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(3000, function(params) {
+  console.log('Server started: http://localhost:3000');
+});
